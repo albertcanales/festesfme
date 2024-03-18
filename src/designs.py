805f04de2a6e92019176b7c92188e0d956cd16f5
Albert Canales Ros <albertcanalesros@gmail.com>
2024-03-18 23:07:24 +0100

    Script llegir dissenys i escriure'ls a samarretes

--- src/designs.py
+++ src/designs.py
@@ -1,1 +1,14 @@
-print("Hello World!")
+import os
+import os.path as path
+import shutil
+
+for static_item in os.listdir("static/"):
+	if path.isdir(f"static/{static_item}") and static_item.startswith("festes"):
+
+		os.makedirs(f"static/{static_item}/samarretes", exist_ok=True)
+
+		for design in os.listdir(f"static/{static_item}/dissenys"):
+			shutil.copyfile(
+				f"static/{static_item}/dissenys/{design}",
+				f"static/{static_item}/samarretes/{design}"
+			)
