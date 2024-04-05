acca456c0dbbc1c0730eb85861b76e27f16e687d
Albert Canales Ros <albertcanalesros@gmail.com>
2024-04-05 23:23:27 +0200

    Add logging and verbosity parameter

--- src/designs.py
+++ src/designs.py
@@ -1,37 +1,67 @@
 import os
 import os.path as path
 from PIL import Image, ImageDraw
+import argparse
+import logging
 import shutil
 
+parser = argparse.ArgumentParser()
+parser.add_argument('-v', '--verbose', action='count', default=0, help="Set verbosity from 0 to 2")
+args = parser.parse_args()
+
+logger = logging.getLogger("designs")
+logger.setLevel((3-min(args.verbose,2))*10)
+logger_handler = logging.StreamHandler()
+logger_handler.setFormatter(logging.Formatter("%(levelname)s: %(message)s"))
+logger.addHandler(logger_handler)
 
 BLANK_TSHIRT = "static/blank_tshirt.png"
 BOX_POINTS = [(190, 100), (410, 400)]
 
+
 box_origin = (BOX_POINTS[0][0], BOX_POINTS[0][1])
 box_size = (BOX_POINTS[1][0]-BOX_POINTS[0][0], BOX_POINTS[1][1]-BOX_POINTS[0][1])
+logger.debug(f"Box origin: { box_origin }")
+logger.debug(f"Box size: { box_size }")
+
 blank_tshirt = Image.open(BLANK_TSHIRT).convert("RGBA")
+logger.info("Successfully loaded blank t-shirt image")
 
 def get_design_size(design_size: tuple[int, int], box_size: tuple[int, int]) -> tuple[int, int]:
 	if design_size[1]/design_size[0] >= box_size[1]/box_size[0]:
 		return (int(box_size[1]/design_size[1]*design_size[0]), box_size[1])
 	return (box_size[0], int(box_size[0]/design_size[0]*design_size[1]))
 
-def create_tshirt(design: str, output_file: str) -> None:
+def create_tshirt(src_dir: str, design_file: str, output_file: str) -> None:
 	tshirt = blank_tshirt.copy()
-	design = Image.open(design).convert("RGBA")
-	resized_design = design.resize(get_design_size(design.size, box_size))
+	design = Image.open(f"{ src_dir }/{ design_file }").convert("RGBA")
+
+	design_size = get_design_size(design.size, box_size)
+	logger.debug(f"Design size for { design_file }: { design_size }")
+
+	resized_design = design.resize(design_size)
 
 	tshirt.paste(resized_design, box_origin, resized_design)
 
+	logger.info(f"Saving t-shirt with design to { output_file }")
 	tshirt.save(output_file, "PNG")
 
+logger.info("Scanning generacions in 'dissenys/'")
 for generacio_dir in os.listdir("dissenys/"):
-	if path.isdir(f"dissenys/{generacio_dir}") and generacio_dir.startswith("festes"):
 
+	logger.info(f"Checking if { generacio_dir } is generació")
+	if path.isdir(f"dissenys/{generacio_dir}") and generacio_dir.startswith("festes"):
+		logger.info(f"Found generació in { generacio_dir }")
+
+		logger.info(f"Creating camisetes directory for generacio { generacio_dir }")
 		os.makedirs(f"static/{generacio_dir}/samarretes", exist_ok=True)
 
+		logger.info(f"Scanning dissenys for generació { generacio_dir }")
 		for design in os.listdir(f"dissenys/{generacio_dir}"):
+
+			logger.info(f"Creating t-shirt out of design { design }")
 			create_tshirt(
-				f"dissenys/{generacio_dir}/{design}",
+				f"dissenys/{generacio_dir}",
+				f"{design}",
 				f"static/{generacio_dir}/samarretes/{design}"
 			)
