94927bacd1470edff3574b118312f18083cef4bd
Albert Canales Ros <albertcanalesros@gmail.com>
2024-04-05 23:53:59 +0200

    Center design position

--- src/designs.py
+++ src/designs.py
@@ -27,21 +27,28 @@ logger.debug(f"Box size: { box_size }")
 blank_tshirt = Image.open(BLANK_TSHIRT).convert("RGBA")
 logger.info("Successfully loaded blank t-shirt image")
 
-def get_design_size(design_size: tuple[int, int], box_size: tuple[int, int]) -> tuple[int, int]:
+def get_design_in_box_size(design_size: tuple[int, int], box_size: tuple[int, int]) -> tuple[int, int]:
 	if design_size[1]/design_size[0] >= box_size[1]/box_size[0]:
 		return (int(box_size[1]/design_size[1]*design_size[0]), box_size[1])
 	return (box_size[0], int(box_size[0]/design_size[0]*design_size[1]))
+
+def get_design_in_box_origin(design_in_box_size: tuple[int, int], box_size: tuple[int, int]) -> tuple[int, int]:
+	if design_in_box_size[1]/design_in_box_size[0] >= box_size[1]/box_size[0]:
+		return (box_origin[0] + (box_size[0]-design_in_box_size[0])//2, box_origin[1])
+	return box_origin
 
 def create_tshirt(src_dir: str, design_file: str, output_file: str) -> None:
 	tshirt = blank_tshirt.copy()
 	design = Image.open(f"{ src_dir }/{ design_file }").convert("RGBA")
 
-	design_size = get_design_size(design.size, box_size)
-	logger.debug(f"Design size for { design_file }: { design_size }")
+	design_in_box_size = get_design_in_box_size(design.size, box_size)
+	logger.debug(f"Design size for { design_file }: { design_in_box_size }")
+	design_in_box_origin = get_design_in_box_origin(design_in_box_size, box_size)
+	logger.debug(f"Design origin for { design_file }: { design_in_box_origin }")
 
-	resized_design = design.resize(design_size)
+	resized_design = design.resize(design_in_box_size)
 
-	tshirt.paste(resized_design, box_origin, resized_design)
+	tshirt.paste(resized_design, design_in_box_origin, resized_design)
 
 	logger.info(f"Saving t-shirt with design to { output_file }")
 	tshirt.save(output_file, "PNG")
